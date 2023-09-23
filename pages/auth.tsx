d6bb60220827441f77a36c22873f6b49258253ad
suha <gusuhawoo@gmail.com>
2023-09-23 13:55:09 +0900

    MongoDB Connection

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Input from '@/components/Input';
 import { useCallback, useState } from 'react';
 
@@ -13,6 +14,18 @@ const Auth = () => {
       currentVariant === 'login' ? 'register' : 'login'
     );
   }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -56,7 +69,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             <p className="text-neutral-500 mt-12">
