02402e2d6d7aad38a574c0f09abc2b29fb7ba097
suha <gusuhawoo@gmail.com>
2023-10-01 20:11:19 +0900

    Add Navbar

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,9 @@
+const NavbarItem = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default NavbarItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import useCurrentUser from '@/hooks/useCurrentUser';
+import React from 'react';
 import { NextPageContext } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
+
+import Navbar from '@/components/Navbar';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,15 +21,12 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function Home() {
-  const { data: user } = useCurrentUser();
+const Home = () => {
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
     </>
   );
-}
+};
+
+export default Home;
