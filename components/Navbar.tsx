cafcede1acceb3f5522d62b3a8f071c777d2ee5b
suha <gusuhawoo@gmail.com>
2023-10-04 22:01:38 +0900

    add account menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,8 @@ import { useCallback, useState } from 'react';
 import MobileMenu from './MobileMenu';
 import NavbarItem from './NavbarItem';
 
-import { BsChevronDown } from 'react-icons/bs';
+import { BsChevronDown, BsSearch, BsBell } from 'react-icons/bs';
+import AccountMenu from './AccountMenu';
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
@@ -51,6 +52,21 @@ const Navbar = () => {
           <p className="text-white text-sm">Browse</p>
           <BsChevronDown className="text-white transition" />
           <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BsBell />
+          </div>
+          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="" />
+            </div>
+            <BsChevronDown className="text-white transition" />
+            <AccountMenu visible />
+          </div>
         </div>
       </div>
     </nav>
