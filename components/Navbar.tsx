58c2fad9535af08643382a7e32e0652382e496d3
suha <gusuhawoo@gmail.com>
2023-10-09 21:23:38 +0900

    NETFLIX clone coding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,32 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import MobileMenu from './MobileMenu';
 import NavbarItem from './NavbarItem';
 
 import { BsChevronDown, BsSearch, BsBell } from 'react-icons/bs';
 import AccountMenu from './AccountMenu';
 
+const TOP_OFFSET = 66;
+
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const toggleMobileMenu = useCallback(() => {
     setShowMobileMenu((current) => !current);
@@ -20,7 +39,7 @@ const Navbar = () => {
   return (
     <nav className="w-full fixed z-40">
       <div
-        className="
+        className={`
           px-4
           me:px-16
           py-6
@@ -29,9 +48,8 @@ const Navbar = () => {
           items-center
           transition
           duration-500
-          bg-zinc-900
-          bg-opacity-90
-        "
+          ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}
+        `}
       >
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
         <div
@@ -55,7 +73,11 @@ const Navbar = () => {
           className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
         >
           <p className="text-white text-sm">Browse</p>
-          <BsChevronDown className="text-white transition" />
+          <BsChevronDown
+            className={`text-white transition ${
+              showMobileMenu ? 'rotate-180' : 'rotate-0'
+            }`}
+          />
           <MobileMenu visible={showMobileMenu} />
         </div>
         <div className="flex flex-row ml-auto gap-7 items-center">
